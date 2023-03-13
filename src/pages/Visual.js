3d79fb7b47a872d097e21bbaec5e6827079b0d7b
hj.over <dhfldhs0707@gmail.com>
2023-03-13 18:18:43 +0900

    중간 저장

--- src/pages/Visual.js
+++ src/pages/Visual.js
@@ -14,17 +14,38 @@ const Visual = () => {
   const box = useRef(null);
   return (
     <div className="visual scroll">
-      <div className="inner">
+      <div className="inner flex justify-between ">
         {/* 첫인상 남기기(사진/대표단어 ) */}
         <img
           src={`${path}/images/me.jpg`}
           alt="visual"
-          style={{ width: "573px", height: "700px" }}
+          style={{ width: "550px", height: "700px" }}
         />
-        <p className="text-[50px] text-teal-700 text-right">
+        <p
+          className="text-[48px] text-teal-700 text-right"
+          data-aos="fade-right"
+          data-aos-duration="600"
+          data-aos-delay="600"
+          style={{ width: "600px", height: "700px" }}
+        >
           Publisher & Frontend Developer
+          <br />
+          <span className="text-[38px] opacity-70">
+            동물과 책을 사랑하는 퍼블리셔 <br />
+            이효정입니다.
+          </span>
+          <span className="flex justify-end">
+            <img
+              src={`${path}/images/write_icon.png`}
+              alt="book"
+              className="w-16 h-16"
+              onClick={() => {
+                <a href=""></a>;
+              }}
+              style={{ cursor: "pointer" }}
+            />
+          </span>
         </p>
-        {/* <img src="/images/visual.png" alt="visual" /> */}
         {/* Anime 샘플 */}
         <div
           ref={box}
